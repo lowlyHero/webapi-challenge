57f9fedcd64cecbb8b536a7023392defc504018c
lowlyHero <e.nunn1009@gmail.com>
2019-06-21 15:20:28 -0400

    projectsRouter needs small tweaks, moving onto actionRouter for times sake since making those small adjustments should not take long

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -21,15 +21,103 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const projs = await Proj.getProjectActions(req.params.id);
-        res.status(200).json(projs);
+        if(projs) {
+          res.status(200).json(projs);
+        } else {
+            res.status(404).json({
+                message: 'This project could not be found. Please try again.'
+            })
+        }
     } catch(error) {
         console.log(error);
         res.status(500).json({
-            message: 'There was an error retrieving the data.'
+            message: 'Internal error. Cannot retrieve data'
         })
     }
 });
 
 //============ POST ================
+
+router.post('/', async (req, res) => {
+    try {
+        const projs = await Proj.insert(req.body);
+        res.status(201).json(projs);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error. Cannot add data'
+        })
+    }
+});
+
+router.post('/:id', async (req, res) => {
+    try {
+        const projs = await Proj.insert(req.body);
+        if(projs) {
+          res.status(201).json(projs);
+        } else {
+            res.status(404).json({
+                message: 'This project could not be found. Please try again.'
+            })
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error. Cannot add data'
+        })
+    }
+});
+
+// ========= PUT ===============
+
+router.put('/', async (req, res) => {
+    try {
+        const projs = await Proj.update(req.params.id);
+        res.status(200).json(projs);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error. Cannot update data'
+        })
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const projs = await Proj.update(req.params.id);
+        if(projs) {
+          res.status(200).json(projs);
+        } else {
+            res.status(404).json({
+                message: 'This project could not be found. Please try again.'
+            })
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error. Cannot update data'
+        })
+    }
+});
+
+//========== DELETE ============
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const delId = await Proj.remove(req.params.id);
+        if(delId <= 0) {
+          res.status(200).json(projs);
+        } else {
+            res.status(404).json({
+                message: 'This project could not be found. Please try again.'
+            })
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error. Cannot remove data'
+        })
+    }
+});
 
 module.exports = projectRouter;
