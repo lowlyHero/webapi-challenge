27e3cde05f6d71bb4dcc06d7a3bd65b9803cc88d
lowlyHero <e.nunn1009@gmail.com>
2019-06-21 15:25:54 -0400

    actionRouter templated

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -1,5 +1,55 @@
 const express = require('express');
 
+const Action = require('./actionModel');
+
 const router = express.Router();
+
+// ======== GET =============
+
+router.get('/', async (req, res) => {
+    try {
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error. Cannot retrieve data.'
+        })
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error. Cannot retrieve data.'
+        })
+    }
+});
+
+// ======= POST ============
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error. Cannot retrieve data.'
+        })
+    }
+});
+
+router.post('/:id', async (req, res) => {
+    try {
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error. Cannot retrieve data.'
+        })
+    }
+});
 
 module.exports = actionRouter;
