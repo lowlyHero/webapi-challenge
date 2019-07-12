3690ae6656373bb44598945867433104910e4418
lowlyHero <e.nunn1009@gmail.com>
2019-07-12 13:58:54 -0400

    CRUD operations complete and functional. Starting on stretch

---
+++ data/helpers/actionRouter.js
@@ -0,0 +1,80 @@
+const express = require('express');
+
+const Action = require('./actionModel');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const action = await Action.get();
+        res.status(200).json(action);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal error. Cannot retrieve data.' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const action = await Action.get(req.params.id);
+        if(action) {
+            res.status(200).json(action)
+        } else {
+            res.status(404).json({ message: 'Action not found. Please try again.' });
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal error. Cannot retrieve data.' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const action = await Action.insert(req.body);
+        res.status(201).json(action);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal error. Cannot add data.' });
+    }
+});
+
+router.post('/:id', async (req, res) => {
+    try {
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal error. Cannot add data.' });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const action = await Action.update(req.params.id, req.body);
+        if(action) {
+            res.status(200).json(action);
+        } else {
+            res.status(404).json({ message: 'Unable to update action. Please try again.' });
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal error. Cannot update data.' });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const delAction = await Action.remove(req.params.id);
+        if(delAction <= 0) {
+            res.status(200).json({
+                message: 'Successfully deleted action.'
+            })
+        } else {
+            res.status(404).json({ message: 'Action not found. Please try again.' });
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal error. Cannot remove data.' });
+    }
+});
+
+module.exports = router;
